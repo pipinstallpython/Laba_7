c189282af1f26e081ca499014ab40e0f05f0140e
Gamid <dim.u7kin@yandex.ru>
2022-12-17 16:59:52 +0300

    'adds'

--- Pycharm/ind_2.py
+++ Pycharm/ind_2.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
     print(max(lst_number))
 
     # сумма до последнего положительного
-    for i in range(len(lst_number) - 1, -1, -1):
-        if lst_number[i] > 0:
-            print(sum(lst_number[:i]))
+    lst = lst_number[::-1]
+    for i, var in enumerate(lst):
+        if var >= 0:
+            print(sum(lst[i::]))
             break
 
     # сжатие списка
